f1cf189e131c081d59080f4c076b33a35a0cf5d1
roman-bg <61371346+roman-bg@users.noreply.github.com>
2020-11-26 13:14:18 +0200

    Add files via upload

---
+++ precache-manifest.680a7c6e2f67e80c7e3efab9e3d133ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71bee7d5df4e6a191053e05c727f40b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ef2066b1357003ba19a",
+    "url": "./static/css/main.14c2df8d.chunk.css"
+  },
+  {
+    "revision": "9cba7f469355afa28f97",
+    "url": "./static/js/2.7e8a5a35.chunk.js"
+  },
+  {
+    "revision": "21c75a98ff1b0cc08c27fd17e11b8c68",
+    "url": "./static/js/2.7e8a5a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef2066b1357003ba19a",
+    "url": "./static/js/main.636782b9.chunk.js"
+  },
+  {
+    "revision": "e8c966f0ddc0a4476bec",
+    "url": "./static/js/runtime-main.7d29428d.js"
+  }
+]);
